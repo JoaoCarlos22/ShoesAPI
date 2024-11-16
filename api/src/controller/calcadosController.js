421466d1c1a3304b134499f872d0bca6a224af75
João Carlos <jcarlosguimaraes13@gmail.com>
2024-11-15 22:18:20 -0400

    Definição da função getCalçados

--- api/src/controller/calcadosController.js
+++ api/src/controller/calcadosController.js
@@ -1,4 +1,12 @@
 const Calçado = require("../models/calcadoModel.js");
 
 
+const getCalçados = async (req, res) => {
+    try{
+        const calçados = await Calçado.find({});
+        res.status(200).json(empresas)
+    }catch(error){
+        res.status(500).json({message: "Erro ao buscar os calçados existentes!"})
+    }
+}
 
